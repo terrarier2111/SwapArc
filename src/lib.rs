721e67b3ed58523d78a810e5abe0b0b1de95520a
threadexception <hannes.gaumann@outlook.de>
2023-02-19 12:13:51 +0100

    Fix issue in raw full guard

--- src/lib.rs
+++ src/lib.rs
@@ -442,25 +442,24 @@ impl<T: Send + Sync, D: DataPtrConvert<T>, const METADATA_BITS: u32>
         // release the reference we hold
         match src {
             RefSource::Curr => {
-                let ref_cnt = self.parent.curr_ref_cnt.fetch_sub(1, Ordering::AcqRel);
+                let ref_cnt = self.curr_ref_cnt.fetch_sub(1, Ordering::AcqRel);
                 // Note: we only perform implicit updates when `OTHER_UPDATE` is set and there
                 // are no other references alive as `OTHER_UPDATE` signals that there is still
                 // an update pending and it would be useless to try to perform an implicit
                 // update if no update is pending.
                 if ref_cnt == SwapArcAnyMeta::<T, D, METADATA_BITS>::OTHER_UPDATE + 1 {
-                    self.parent.try_update_curr();
+                    self.try_update_curr();
                 }
             }
             RefSource::Intermediate => {
                 let ref_cnt = self
-                    .parent
                     .intermediate_ref_cnt
                     .fetch_sub(1, Ordering::AcqRel);
                 // fast-rejection path to ensure we are only trying to update if it's worth it:
                 // first check if there are no other references alive, then check if there are
                 // still updates pending.
-                if ref_cnt == 1 && !self.parent.updated.load(Ordering::Relaxed).is_null() {
-                    self.parent.try_update_intermediate();
+                if ref_cnt == 1 && !self.updated.load(Ordering::Relaxed).is_null() {
+                    self.try_update_intermediate();
                 }
             }
         }
